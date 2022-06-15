8c3586716f93e8772bb34abbb66010f4a46d508f
Yasin Khan <yasinkhan500@gmail.com>
2022-06-14 20:53:48 -0700

    stache

--- src/components/App.js
+++ src/components/App.js
@@ -5,25 +5,30 @@ import '../styles/App.css';
 
 export default function App() {
 	const [posts, setPosts] = useState([]);
+	const [currPosts, setCurrPosts] = useState([]);
 	const [loading, setLoading] = useState(false);
 	const [currentPage, setCurrentPage] = useState(1);
 	const [postsPerPage] = useState(5);
-
-	const endIdx = currentPage * postsPerPage;
-	const startIdx = endIdx - postsPerPage;
-	const currentPosts = posts.slice(startIdx, endIdx);
 
 	const getPosts = async () => {
 		setLoading(true);
 		const res = await fetch('https://jsonplaceholder.typicode.com/posts');
 		const data = await res.json();
 		setPosts(data);
+		setCurrPosts(data.slice(0, 5));
 		setLoading(false);
 	};
 
 	useEffect(() => {
 		getPosts();
 	}, []);
+
+	useEffect(() => {
+		const endIdx = currentPage * postsPerPage;
+		const startIdx = endIdx - postsPerPage;
+		const postsCopy = [...posts];
+		setCurrPosts(postsCopy.slice(startIdx, endIdx));
+	}, [currentPage, posts, postsPerPage]);
 
 	const goToNextPage = () => {
 		setCurrentPage(currPage => currPage + 1);
@@ -33,10 +38,6 @@ export default function App() {
 		setCurrentPage(currPage => currPage - 1);
 	};
 
-	const handlePagination = pageNum => {
-		setCurrentPage(pageNum);
-	};
-
 	const getPaginationGroup = () => {
 		let start = Math.floor((currentPage - 1) / postsPerPage) * postsPerPage;
 		return new Array(postsPerPage).fill().map((_, idx) => start + idx + 1);
@@ -44,11 +45,11 @@ export default function App() {
 
 	return (
 		<div className='app'>
-			<Posts posts={currentPosts} loading={loading} />
+			<Posts posts={currPosts} loading={loading} />
 			<Pagination
 				postsPerPage={postsPerPage}
 				totalPosts={posts.length}
-				paginate={handlePagination}
+				paginate={pageNum => setCurrentPage(pageNum)}
 				currentPage={currentPage}
 				goToPrevPage={goToPrevPage}
 				goToNextPage={goToNextPage}
